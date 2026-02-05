6fc0019ad32cc1388ad06479ae3aca4e2f206653
JPrevilon <joshuaprevilon13@gmail.com>
2026-02-05 16:37:54 -0500

    Complete Blog Site lab – all tests passing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import blogData from "../data/blog";
+// App component
+// Root component of the blog site
+// Connects Header, About, and ArticleList components
+// Imports blog data and passes it down as props
 
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import blog from "../data/blog.js";
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blog.name} />
+      <About image={blog.image} about={blog.about} />
+      <ArticleList posts={blog.posts} />
     </div>
   );
 }
